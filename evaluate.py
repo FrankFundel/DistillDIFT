2cddbcf9d6b4b717a46e4a47751d35dbe12c8c58
Frank <frank.fundel@predori.com>
2023-12-04 13:57:20 +0100

    update evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,3 @@
-# load model
-# load datasets
-# run evaluation
-# calculate values
-# print results
-
 import torch
 import tqdm
 import argparse
@@ -28,17 +22,19 @@ elif model == 'sd1.4':
 
 device = torch.device(device)
 
-for (source_images, target_images, source_points, target_points) in tqdm.tqdm(dataloader):
-    source_images, target_images = source_images.to(device), target_images.to(device)
-    source_points, target_points = source_points.to(device), target_points.to(device)
-    
-    # points must be to be [x, y] tensors
-    source_size = source_images.shape
-    target_size = target_images.shape
+def evaluate(dataloader):
+    for (source_images, target_images, source_points, target_points) in tqdm.tqdm(dataloader):
+        # load data on device
+        source_images, target_images = source_images.to(device), target_images.to(device)
+        source_points, target_points = source_points.to(device), target_points.to(device)
+        
+        # points must be to be [x, y] tensors
+        source_size = source_images.shape
+        target_size = target_images.shape
 
-    # swap from (x, y) to (y, x)
-    source_points = torch.flip(source_points, 1)
-    target_points = torch.flip(target_points, 1)
+        # swap from (x, y) to (y, x)
+        source_points = torch.flip(source_points, 1)
+        target_points = torch.flip(target_points, 1)
 
-    # run through model
-    predicted_points = model(source_images)
+        # run through model
+        predicted_points = model(source_images)
--- utils/utils.py
+++ utils/utils.py
@@ -12,6 +12,14 @@ def normalize_features(features):
     features = features / torch.linalg.norm(features, dim=-1)[:, :, None]
     return features
 
+def points_to_idxs(points, load_size):
+    points_y = points[:, 0]
+    points_y = torch.clip(points_y, 0, load_size[1]-1)
+    points_x = points[:, 1]
+    points_x = torch.clip(points_x, 0, load_size[0]-1)
+    idx = load_size[1] * torch.round(points_y) + torch.round(points_x)
+    return idx
+
 def cosine_similarity(source_features, target_features, flatten=True, normalize=True):
     if flatten:
         source_features = flatten_features(source_features)
@@ -19,32 +27,38 @@ def cosine_similarity(source_features, target_features, flatten=True, normalize=
     if normalize:
         source_features = normalize_features(source_features)
         target_features = normalize_features(target_features)
-    sim = torch.matmul(source_features, target_features.permute((0, 2, 1)))
-    return sim
+    similarity = torch.matmul(source_features, target_features.permute((0, 2, 1)))
+    return similarity
 
-def get_correspondences(source_features, target_features, source_points, output_size, load_size):
-    """
-    Precompute nearest neighbor of source_points in img1 to target_points in img2.
-    """
-    img1_feats = torch.nn.functional.interpolate(img1_feats, load_size, mode="bilinear")
-    img2_feats = torch.nn.functional.interpolate(img2_feats, load_size, mode="bilinear")
+def compute_pck(predicted_points, target_points, load_size, pck_threshold=0.1, target_bounding_box=None):
+    distances = torch.linalg.norm(predicted_points - target_points, axis=-1)
+    if target_bounding_box is None:
+        pck = distances <= pck_threshold * max(load_size)
+    else:
+        left, top, right, bottom = target_bounding_box
+        pck = distances <= pck_threshold * max(right-left, bottom-top)
+    return pck.mean()
+
+
+def get_correspondences(source_features, target_features, source_points, load_size):
+    source_features = torch.nn.functional.interpolate(source_features, load_size, mode="bilinear")
+    target_features = torch.nn.functional.interpolate(target_features, load_size, mode="bilinear")
 
     source_idx = torch.from_numpy(points_to_idxs(source_points, load_size)).long()
     # Select source_points in the flattened (w, h) dimension as source_idx
-    img1_feats = flatten_feats(img1_feats)
-    img2_feats = flatten_feats(img2_feats)
-    img1_feats = img1_feats[:, source_idx, :]
-    img1_feats = normalize_feats(img1_feats)
-    img2_feats = normalize_feats(img2_feats)
-    sims = torch.matmul(img1_feats, img2_feats.permute((0, 2, 1)))
+    source_features = flatten_features(source_features)
+    target_features = flatten_features(target_features)
+    source_features = source_features[:, source_idx, :]
+    source_features = normalize_features(source_features)
+    target_features = normalize_features(target_features)
+    similarity = torch.matmul(source_features, target_features.permute((0, 2, 1)))
 
     # Find nn_correspondences but with points1 = source_points
-    num_pixels = int(math.sqrt(sims.shape[-1]))
-    points2 = sims.argmax(dim=-1)
-    points2_x = points2 % num_pixels
-    points2_y = points2 // num_pixels
-    points2 = torch.stack([points2_y, points2_x], dim=-1)
+    num_pixels = int(torch.sqrt(similarity.shape[-1]))
+    predicted_points = similarity.argmax(dim=-1)
+    predicted_points_x = predicted_points % num_pixels
+    predicted_points_y = predicted_points // num_pixels
+    predicted_points = torch.stack([predicted_points_x, predicted_points_y], dim=-1)
 
-    points1 = torch.from_numpy(source_points)
-    points2 = points2[0]
-    return points1, points2
+    predicted_points = predicted_points[0]
+    return predicted_points
